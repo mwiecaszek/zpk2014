ce73b7f3247b5a9d83218cc3211bd4563065d7d1
gaapa <gapinski.raf@gmail.com>
2015-03-04 23:29:50 +0100

    Praca domowa 1 cd.

--- home/rgapinski/z1/pod.cpp
+++ home/rgapinski/z1/pod.cpp
@@ -4,9 +4,15 @@ using namespace std;
 int main(){
     int a, b, k, x1, x2, ile_bylo;
     cin >> a >> b >> k;
-     if(b == a && b % k == 0)
+    if(b == a && b % k == 0){
         ile_bylo = 1;
-    if(b < a | (b == a && b % k != 0 ) )
+        cout << ile_bylo << endl;
+    }
+    if(b == a && b % k != 0){
+        ile_bylo = 0;
+        cout << ile_bylo << endl;
+    }
+    if(b < a)
         cout << "Niepoprawne dane" << endl;
     if(b > a){
         if(a % k == 0)
@@ -15,10 +21,8 @@ int main(){
             x1 = (a/k + 1);
         x2 = b/k;
         ile_bylo = x2 - x1 + 1;
+        cout << ile_bylo << endl;
     }
-    if( k > b )
-        ile_bylo = 0;
-    cout << ile_bylo << endl;
 }
 
 
