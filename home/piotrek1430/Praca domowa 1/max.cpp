777bb411a768d30bfbec2e5c39ebb10a1436d646
piotrek1430 <piotrek1430@wp.pl>
2015-03-04 01:04:26 +0100

    Praca domowa 1

---
+++ home/piotrek1430/Praca domowa 1/max.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long int a,b;
+    cin >> a >> b;
+    if (a>b)
+        {
+    cout  << a << endl;
+        }else
+        {
+            cout  << b << endl;
+        }
+}
---
+++ home/piotrek1430/Praca domowa 1/pot.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned int a, wynik;
+    cin >> a;
+    wynik=1;
+    while(wynik<=a){
+
+    cout << wynik<< endl;
+    wynik=wynik*2;
+    }
+}
