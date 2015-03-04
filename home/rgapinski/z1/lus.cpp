d86ec2085166a8b157e53e3b67111a73a76e6ee8
gaapa <gapinski.raf@gmail.com>
2015-03-04 23:43:24 +0100

    cd
    poprawka stylistyczna ;)

--- home/rgapinski/z1/lus.cpp
+++ home/rgapinski/z1/lus.cpp
@@ -8,9 +8,9 @@ int main() {
     int lustrzana = 0;
     cin >> liczba;
     while (liczba > 0){
-    lustrzana = 10*lustrzana + liczba%10;
-    z = liczba/10;
-    liczba = floor(z);
+        lustrzana = 10*lustrzana + liczba%10;
+        z = liczba/10;
+        liczba = floor(z);
     }
     cout << lustrzana << endl;
 }
