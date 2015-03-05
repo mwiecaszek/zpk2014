ab33dac55a259073cf931fffad28e8209673c0c0
TomaszGusciora <tgusciora@student.uw.edu.pl>
2015-03-05 17:57:55 +0100

    SIL
    SIL

---
+++ home/TomaszGusciora/z2/SIL.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int x,n;
+ x=1;
+ cin >> n;
+ if (n == 0) x=1;
+ else for (int i=1;i<n+1;i++)
+ {
+     x *= i;
+ };
+  cout << x << endl;
+}
