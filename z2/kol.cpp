7242061fb7dc6c98e58d87d35468b7d6ce830fdc
mkalbarczyk <kalbarczykmagda@gmail.com>
2015-03-11 00:19:34 +0100

    z2

---
+++ z2/kol.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+
+    double r;
+    cin>>r;
+
+    cout<<fixed<<setprecision(3)<<M_PI*r*r<<endl;
+    cout<<fixed<<setprecision(3)<<M_PI*2*r<<endl;
+
+}
