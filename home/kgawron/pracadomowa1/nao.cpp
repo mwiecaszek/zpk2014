c941efdfb67830fe0bf4521029db09d40626cdf8
kgawron <karolina.gawron@student.uw.edu.pl>
2015-03-11 21:13:29 +0100

    Revert "praca_domowa1"
    This reverts commit 60078f5b2ceb3aab696ae5970f4553dba0c63224.

--- home/kgawron/pracadomowa1/nao.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a, b, c;
-    cin >> a >> b >> c;
-    cout << "Wczytane liczby to: " << a << " " << b << " " << c <<  endl;
-        << "Wczytane liczby w odwrotnej kolejnosci to: " << c << " " << b << " " << a << endl;
-}
--- home/kgawron/pracadomowa1/poc.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Mount Everest - 8850 m n.p.m." << endl
-         << "McKinley - 6194 m n.p.m." << endl
-         << "Mont Blanc - 4810 m n.p.m." << endl;
-}
