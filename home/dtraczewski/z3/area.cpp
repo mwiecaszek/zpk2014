8520bbc207513de2a584004ce19127a57b41f23d
dtraczewski <dtraczewski@student.uw.edu.pl>
2015-03-13 19:07:17 +0100

    Praca domowa 3
    Praca domowa 3

--- home/dtraczewski/z3/area.cpp
+++ home/dtraczewski/z3/area.cpp
@@ -4,27 +4,38 @@ using namespace std;
 
 #include "point.h"
 
-double area_under(Point a, Point b)
+double lengthAB(Point a, Point b)
 {
-    return (b.getX() - a.getX()) * (a.getY() + b.getY()) / 2.0;
+    return sqrt(pow(a.getX() - b.getX(), 2) + pow(a.getY() - b.getY(), 2) + pow(a.getZ() - b.getZ(), 2));
 }
 
 double area(Point a, Point b, Point c)
 {
-    return fabs(area_under(a,b) + area_under(b,c) + area_under(c,a));
+    double _a = lengthAB(a, b);
+    double _b = lengthAB(a, c);
+    double _c = lengthAB(c, b);
+    double p = (_a + _b + _c) / 2;
+
+    return sqrt(p * (p - _a) * (p - _b) * (p - _c));
 }
 
 int main()
 {
     Point p;
-    Point q(2.0, 2.0, 1.0);
-    Point r(0.0, 3.0, 4.5);
+    Point q;
+    Point r;
 
     cout << "Podaj wspolrzedne punktu p: ";
-    cin >> p;
+    cin >> p; cout << endl;
+
+    cout << "Podaj wspolrzedne punktu q: ";
+    cin >> q; cout << endl;
+
+    cout << "Podaj wspolrzedne punktu r: ";
+    cin >> r; cout << endl;
 
     cout << "Punkty: " << p << " " << q << " " << r << endl;
-    cout << "Pole: " << area(p,q,r) << endl;
+    cout << "Pole: " << area(p, q, r) << endl;
 
     return 0;
 }
