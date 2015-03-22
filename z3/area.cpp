362e6b65b0e5f9a9d92e7a79034847a4daf5ccaa
pputowski <pputowski@student.uw.edu.pl>
2015-03-22 21:47:59 +0100

    Revert "Revert "Praca domowa 3""
    This reverts commit 4b721539625d3c8d4e05d3f54337173c0f1c062c.

--- z3/area.cpp
+++ z3/area.cpp
@@ -14,17 +14,36 @@ double area(Point a, Point b, Point c)
     return fabs(area_under(a,b) + area_under(b,c) + area_under(c,a));
 }
 
+double pole( Point a, Point b, Point c )
+{
+    double AB, BC, CA, p;
+    AB = sqrt( ( b.getX() - a.getX() ) * ( b.getX() - a.getX() ) + ( b.getY() - a.getY() ) * ( b.getY() - a.getY() )
+               + ( b.getZ() - a.getZ() ) * ( b.getZ() - a.getZ() ) );
+    BC = sqrt( ( c.getX() - b.getX() ) * ( c.getX() - b.getX() ) + ( c.getY() - b.getY() ) * ( c.getY() - b.getY() )
+               + ( c.getZ() - b.getZ() ) * ( c.getZ() - b.getZ() ) );
+    CA = sqrt( ( a.getX() - c.getX() ) * ( a.getX() - c.getX() ) + ( a.getY() - c.getY() ) * ( a.getY() - c.getY() )
+               + ( a.getZ() - c.getZ() ) * ( a.getZ() - c.getZ() ) );
+    p = ( AB + BC + CA ) / 2;
+    return sqrt( p * ( p - AB ) * ( p - BC ) * ( p - CA ) );
+}
+
 int main()
 {
-    Point p;
-    Point q(2.0, 2.0);
-    Point r(0.0, 3.0);
+    Point a;
+    Point b;
+    Point c;
 
-    cout << "Podaj wspolrzedne punktu p: ";
-    cin >> p;
+    cout << "Podaj wspolrzedne punktu a: ";
+    cin >> a;
 
-    cout << "Punkty: " << p << " " << q << " " << r << endl;
-    cout << "Pole: " << area(p,q,r) << endl;
+    cout << "Podaj wspolrzedne punktu b: ";
+    cin >> b;
+
+    cout << "Podaj wspolrzedne punktu c: ";
+    cin >> c;
+
+    cout << "Punkty: " << a << " " << b << " " << c << endl;
+    cout << "Pole: " << pole( a, b, c ) << endl;
 
     return 0;
 }
