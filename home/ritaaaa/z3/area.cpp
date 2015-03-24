90510e0363272cebee840b8f8c632801feb53833
ritaaaa <f.rita91@gmail.com>
2015-03-24 17:17:27 +0100

    praca domowa 3

---
+++ home/ritaaaa/z3/area.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+#include "point.h"
+
+double dlugosc(Point a, Point b)
+{
+    return sqrt(pow(a.getX() - b.getX(), 2) + pow(a.getY() - b.getY(), 2) + pow(a.getZ() - b.getZ(), 2));
+}
+
+double area(Point a, Point b, Point c)
+{
+    double AB = dlugosc(a,b);
+    double BC = dlugosc(a,c);
+    double CB = dlugosc(c,b);
+    double p = (AB+BC+CB)/2;
+
+    return sqrt(p * (p-AB) * (p-BC) * (p-CB));
+}
+
+int main()
+{
+    Point p;
+    Point q;
+    Point r;
+
+    cout << "Podaj wspolrzedne punktu p: ";
+    cin >> p;
+    cout << "Podaj wspolrzedne punktu q: ";
+    cin >> q;
+    cout << "Podaj wspolrzedne punktu r: ";
+    cin >> r;
+    cout << "Punkty: " << p << " " << q << " " << r << endl;
+    cout << "Pole: " << area(p,q,r) << endl;
+
+    return 0;
+}
