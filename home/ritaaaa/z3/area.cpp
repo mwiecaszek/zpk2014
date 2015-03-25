ad1efefb7fa34f3594153e2a8467081d618cd28f
ritaaaa <f.rita91@gmail.com>
2015-03-25 11:56:24 +0100

    praca domowa 3

--- home/ritaaaa/z3/area.cpp
+++ home/ritaaaa/z3/area.cpp
@@ -4,19 +4,17 @@ using namespace std;
 
 #include "point.h"
 
-double dlugosc(Point a, Point b)
-{
-    return sqrt(pow(a.getX() - b.getX(), 2) + pow(a.getY() - b.getY(), 2) + pow(a.getZ() - b.getZ(), 2));
-}
+
 
 double area(Point a, Point b, Point c)
-{
-    double AB = dlugosc(a,b);
-    double AC = dlugosc(a,c);
-    double CB = dlugosc(c,b);
-    double p = (AB+AC+CB)/2;
+{  double AB,AC,BC,S,P;
+    AB = sqrt(pow(a.getX() - b.getX(), 2) + pow(a.getY() - b.getY(), 2) + pow(a.getZ() - b.getZ(), 2));
+    AC = sqrt(pow(a.getX() - c.getX(), 2) + pow(a.getY() - c.getY(), 2) + pow(a.getZ() - c.getZ(), 2));
+    BC = sqrt(pow(b.getX() - c.getX(), 2) + pow(b.getY() - c.getY(), 2) + pow(b.getZ() - c.getZ(), 2));
 
-    return sqrt(p * (p-AB) * (p-AC) * (p-CB));
+    P = (AB+AC+BC)/2;
+    S=sqrt(P * (P - AB) * (P - AC) * (P - BC));
+    return S;
 }
 
 int main()
@@ -27,10 +25,13 @@ int main()
 
     cout << "Podaj wspolrzedne punktu p: ";
     cin >> p;
+
     cout << "Podaj wspolrzedne punktu q: ";
     cin >> q;
+
     cout << "Podaj wspolrzedne punktu r: ";
     cin >> r;
+
     cout << "Punkty: " << p << " " << q << " " << r << endl;
     cout << "Pole: " << area(p,q,r) << endl;
 
