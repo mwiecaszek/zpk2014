da20b44ce79c135694b23ace61865cd2ce4430a0
MichalDrygala <michal.drygala@gmail.com>
2015-03-25 13:54:09 +0100

    PD3

--- home/MichalDrygala/z3/pd3/point.h
+++ home/MichalDrygala/z3/pd3/point.h
@@ -7,23 +7,26 @@ using namespace std;
 
 class Point
 {
-    double x, y;
+    double x, y, z;
 
 public:
     Point();
-    Point(double, double);
+    Point(double, double, double);
 
     ~Point();
 
     void setX(double);
     void setY(double);
+    void setZ(double);
 
     double getX();
     double getY();
+    double getZ();
 
     friend istream& operator>>(istream&, Point&);
 };
 
 ostream& operator<<(ostream &, Point);
+istream& operator>>(istream &, Point&);
 
 #endif
